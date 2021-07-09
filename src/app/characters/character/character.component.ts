57315abbab8c3527ace9f0f21955e620ee8230d0
Esteban Revelo <84397774+StbanJ@users.noreply.github.com>
2021-07-09 12:23:26 -0500

    App heroku

---
+++ src/app/characters/character/character.component.ts
@@ -0,0 +1,28 @@
+import { CharactersService } from './../../services/characters.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-character',
+  templateUrl: './character.component.html',
+  styleUrls: ['./character.component.scss']
+})
+export class CharacterComponent implements OnInit {
+  character: any;
+
+  constructor(private traer: ActivatedRoute, private service: CharactersService) {
+    const id = this.traer.snapshot.params.id
+   }
+
+  ngOnInit(): void {
+    this.traer.params.subscribe((params)=>{
+      let { id } = params;
+      this.service.getCharacter(id).then((character)=>{
+        this.character = character
+        console.log(this.character)
+      })
+    })
+  }
+}
+
+
